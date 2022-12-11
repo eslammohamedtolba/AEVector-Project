1b5e41a055a17d54343a90db842a6fbef97ea48a
abdo1234510 <92454005+Abdelrahman-Wael-1029@users.noreply.github.com>
2022-12-12 01:43:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,149 +1,165 @@
 #include "bits/stdc++.h"
+
 using namespace std;
+
 template<class T>
-class AEVector{
+class AEVector {
 private:
     int size;
+    int cap;
     T *arr;
 public:
-    class Iterator
-    {
-    public:
-        Iterator();
-        Iterator(const AEVector *pVector, int nIndex): m_pVector(pVector), Indexiter(nIndex){}
-        Iterator &operator++()
-        {
-                ++Indexiter;
-                return *this;
-        }
-        const T &operator*() const
-        {
-            return m_pVector->operator[](Indexiter);
-        }
-        bool operator!=(const Iterator &other)
-        {
-            return (Indexiter != other.Indexiter);
-        }
-        bool operator==(const Iterator &other)
-        {
-            return Indexiter == other.m_nIndex;
-        }
-    private:
+    class Iterator {
         const AEVector *m_pVector;
         int Indexiter = -1;
+    public:
+        Iterator() = default;
+
+        Iterator(const AEVector *pVector, int nIndex) : m_pVector(pVector), Indexiter(nIndex) {}
+
+        Iterator &operator++() {
+            ++Indexiter;
+            return *this;
+        }
+
+        const T &operator*() const {
+            return m_pVector->operator[](Indexiter);
+        }
+
+        bool operator!=(const Iterator &other) {
+            return (Indexiter != other.Indexiter);
+        }
+
+        bool operator==(const Iterator &other) {
+            return Indexiter == other.m_nIndex;
+        }
+
     };
-    AEVector(int newsize=0){
-          this->size=size;
-          arr=new T[newsize];
-          for(int i=0;i<newsize;i++){
-              arr[i]=0;
-          }
-    }
-    AEVector(T* newarr,int n){
-        size=n;
-        arr=new T[n];
-        for(int i=0;i<n;i++) {
-            arr[i]=newarr[i];
+
+    explicit AEVector(int newsize) {
+        size = cap = newsize;
+        arr = new T[newsize];
+        for (int i = 0; i < newsize; i++) {
+            arr[i] = 0;
         }
     }
-    ~AEVector(){
-        size=0;
-        delete []arr;
+
+    explicit AEVector() {
+        this->size = cap = 0;
+        arr = nullptr;
     }
-    AEVector(const AEVector &other){
-       size=other.size;
-       arr=new T[size];
-       for(int i=0;i<size;i++){
-           arr[i]=other.arr[i];
-       }
+
+    AEVector(T *newarr, int n) {
+        size = cap = n;
+        delete[]arr;
+        arr = new T[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = newarr[i];
+        }
     }
-    AEVector& operator=(const AEVector &other){
-        if(this!=&other){
-            size=other.size;
-            delete []arr;
-            arr=new T[size];
-            for(int i=0;i<size;i++){
-                arr[i]=other.arr[i];
+
+    AEVector(const AEVector &other) {
+
+        size = cap = other.size;
+        arr = new T[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = other.arr[i];
+        }
+    }
+
+    ~AEVector() {
+        size = cap = 0;
+        delete arr;
+    }
+
+    AEVector &operator=(const AEVector &other) {
+        if (this != &other) {
+            size = cap = other.size;
+            delete[]arr;
+            arr = new T[size];
+            for (int i = 0; i < size; i++) {
+                arr[i] = other.arr[i];
             }
-        }
-        return*this;
-    }
-    AEVector(const AEVector &&other){
-        size=other.size;
-        arr=new T[size];
-        for(int i=0;i<size;i++){
-            arr[i]=other.arr[i];
-        }
-        arr= nullptr;
-    }
-    AEVector&operator=(const AEVector&&other){
-        if(this!=&other){
-            delete []arr;
-            size=other.size;
-            arr=new T[size];
-            for(int i=0;i<size;i++){
-                arr[i]=other.arr[i];
-            }
-            arr= nullptr;
         }
         return *this;
     }
-    const T& operator[](int index) const
-    {
-        try {
-            if(index<0 || index>=size){
-                throw exception();
+
+    AEVector(const AEVector &&other) noexcept {
+        size = cap = other.size;
+        arr = new T[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = other.arr[i];
+        }
+        other = 0;
+    }
+
+    AEVector &operator=(const AEVector &&other) noexcept {
+        if (this != &other) {
+            delete[]arr;
+            size = cap = other.size;
+            arr = new T[size];
+            for (int i = 0; i < size; i++) {
+                arr[i] = other.arr[i];
             }
-            else{
+
+        }
+
+        other = 0;
+        return *this;
+    }
+
+    T &operator[](int index) const {
+        try {
+            if (index < 0 || index >= size) {
+                throw exception();
+            } else {
                 return arr[index];
             }
         }
-        catch (exception)
-        {
-            cout<<"index out of range"<<endl;
+        catch (exception) {
+            cout << "index out of range" << endl;
         }
     }
-    int push_back(T Item){
-        size++;
-        T *newarr=new T[size];
-        for(int i=0;i<size-1;i++){
-            newarr[i]=arr[i];
+
+    int push_back(T Item) {
+        if (size == cap) {
+            cap = cap * 2;
+            T *newarr = new T[cap];
+            for (int i = 0; i < size; i++) {
+                newarr[i] = arr[i];
+            }
+            delete[]arr;
+            arr = newarr;
         }
-        newarr[size]=Item;
-        delete []arr;
-        arr=newarr;
+        arr[size] = Item;
+        size++;
         return size;
     }
-    T pop_back()
-    {
-        if(size>0){
-            T Item=arr[size-1];
+
+    T pop_back() {
+        if (size > 0) {
+            T Item = arr[size - 1];
             size--;
-            T *newarr=new T[size];
-            for(int i=0;i<size;i++){
-                newarr[i]=arr[i];
-            }
-            delete []arr;
-            arr=newarr;
             return Item;
         }
     }
-    void clear()
-    {
-        size=0;
-        arr= nullptr;
+
+    void clear() {
+        size = 0;
+        delete[]arr;
+        arr = nullptr;
     }
+
     //---------------------------------------------------------------
-    Iterator begin()
-    {
-        return AEVector<T>::Iterator{this,0};
+    Iterator begin() {
+        return AEVector<T>::Iterator{this, 0};
     }
-    Iterator end()
-    {
-        return AEVector<T>::Iterator{this,size};
+
+    Iterator end() {
+        return AEVector<T>::Iterator{this, size};
     }
-    void insert(Iterator iter,T value)
-    {
+
+    void insert(Iterator iter, T value) {
         size++;
         T *newarr=new T[size];int index=0;
         cout<<*this->begin()<<endl;
@@ -159,8 +175,8 @@ public:
         delete arr;
         arr=newarr;
     }
-    void erase(Iterator iter)
-    {
+
+    void erase(Iterator iter) {
         size--;
         T *newarr=new T[size];int index=0;
         for(AEVector<T>::Iterator i=this->begin();i!=this->end();++i){
@@ -171,36 +187,94 @@ public:
         }
         delete arr;
         arr=newarr;
+
     }
-    void erase(Iterator iter1, Iterator iter2);     // Remove items between
-                                                    // iterator 1 <= iterator 2 otherwise do nothing
-                                                    // Throw exception if any iterator outside range
+
+    void erase(Iterator iter1, Iterator iter2){
+
+    }
+
+
+    // Remove items between
+    // iterator 1 <= iterator 2 otherwise do nothing
+    // Throw exception if any iterator outside range
 
     // Iterators 		// Supports *, + and ++ operations at least
-                        // Can use: typedef T* iterator
-                        // Or u can use std::iterator so you can
-                        // apply STL algorithms on XYVector
+    // Can use: typedef T* iterator
+    // Or u can use std::iterator so you can
+    // apply STL algorithms on XYVector
 
     // Comparison operations
-    bool operator==(const AEVector<T>&); // Return true if ==
-    bool operator< (const AEVector<T>&); // Compares item by item
-                                         //Return true if first different item in this is < in other
+    bool operator==(const AEVector<T> &other) { // Return true if ==
+        for (int i = 0; i < min(size, other.Size()); ++i) {
+            if (arr[i] != other[i])return false;
+        }
+        return other.Size() == size;
+    }
+
+    bool operator<(
+            const AEVector<T> &other) {// Compares item by item  //Return true if first different item in this is < in other
+        for (int i = 0; i < min(size, other.Size()); ++i) {
+            if (arr[i] != other[i]) {
+                return arr[i] < other[i];
+            }
+        }
+        return size < other.Size();
+
+    }
 
     // Capacity operations
-    int Size() const;     // Return current size of vec
-    int capacity() const; // Return size of current allocated array
-    int resize();         // Relocate to bigger space
-    bool empty();         // Return true if size is 0
+    int Size() const {  // Return current size of vec
+        return size;
+    }
+
+    int capacity() const { // Return size of current allocated array
+        return cap;
+
+    }
+
+
+    void resize(int newsize) {// Relocate to bigger space
+        T newarray = new T[newsize];
+        if (newsize > cap) {
+            cap = max(2 * cap, newsize);
+        }
+        for (int i = 0; i < size; ++i) {
+            newarray[i] = arr[i];
+        }
+        for (int i = size; i < newsize; ++i) {
+            newarray[i] = 0;
+        }
+        delete[]arr;
+        arr = newarray;
+        newarray = nullptr;
+        delete[]newarray;
+    }
+
+    bool empty() {       // Return true if size is 0
+        return !size;
+    }
 
     // Friends
-    friend ostream& operator << (ostream& out, AEVector<T>other);
+    friend ostream &operator<<(ostream &out, AEVector<T> other) {
+        for (int i = 0; i < other.Size(); ++i) {
+            cout << other[i]<< ' ';
+        }
+    }
 };
+
 //----------------------------------------------------------------------------------------------------------------------
-int main()
-{
-    AEVector<int>t;
-    AEVector<int>::Iterator it=t.begin();
-    t.insert(it,5);
-    t.erase(it);
-    cout<<"Eslam"<<endl;
+int main() {
+    AEVector<int>::Iterator it,n;
+    AEVector<int> arr(5);
+    it=arr.begin();
+    arr.insert(it,5);
+//    for (int i = 0; i <arr.Size(); ++i) {
+//        cout<<arr[i]<<endl;
+//
+//    }
+    cout<<arr;
+
+
 }
+
